0e3f9d2333743f77e120fb185fe03647b33705ce
Kriti Bagga <72100314+kritibagga@users.noreply.github.com>
2023-03-14 16:59:32 -0400

    WeatherApp

---
+++ weather-app/src/Components/getCityName.js
@@ -0,0 +1,21 @@
+export async function getCityLatLng(
+	searchInput,
+	country,
+
+) {
+
+	try {
+		const response = await fetch(`${process.env.REACT_APP_GEO_API_URL}${searchInput},${country}&limit=1&appid=${process.env.REACT_APP_API_KEY}`);
+		const data = await response.json();
+
+		if (data.length === 0) {
+			throw new Error("No location found");
+		}
+
+		const { lat, lon } = data[0];
+
+		return { lat, lon };
+	} catch (error) {
+		console.error(error);
+	}
+}
