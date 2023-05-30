3989d87d50b6f1ef6097014f4a6a3c5f3b09cd86
kriti bagga <bagga.kriti@gmail.com>
2023-05-30 18:14:22 -0400

    created responsive view

--- weather-app/src/Components/getCityName.js
+++ weather-app/src/Components/getCityName.js
@@ -1,11 +1,8 @@
-export async function getCityLatLng(
-	searchInput,
-	country,
-
-) {
-
+export async function getCityLatLng(searchInput, country) {
 	try {
-		const response = await fetch(`${process.env.REACT_APP_GEO_API_URL}${searchInput},${country}&limit=1&appid=${process.env.REACT_APP_API_KEY}`);
+		const response = await fetch(
+			`${process.env.REACT_APP_GEO_API_URL}${searchInput},${country}&limit=1&appid=${process.env.REACT_APP_API_KEY}`
+		);
 		const data = await response.json();
 
 		if (data.length === 0) {
